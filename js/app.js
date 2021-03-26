b0ca7b7890708eee77e6c1c67d09c998d44bdbee
Deyvis <deyvisjaime17@gmail.com>
2021-03-25 19:13:22 -0500

    fondo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const numero = document.querySelector('.numero')
+ 
+
 
 const obtener = document.querySelector('.obtener')
 
@@ -26,75 +28,52 @@ const celular = ()=>{
 
 }
 
-
 obtener.addEventListener('click',(e)=>{
     e.preventDefault()
-    setTimeout(() => {
-    if (celular()) {
-        			window.open(urlMovil+numero.value);
-        		} else {
-        			window.open(urlWeb+numero.value);
-        		}
+    if(!numero.value.length>0){
+        Swal.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: 'Debe ingresar un numero valido',
+            showConfirmButton: false,
+            timer: 1000
+          })
+    }else{
+    Swal.fire({
+        title: 'Buscando Numero en WhatsApp',
+        html: 'Queda <b></b> milisegundos',
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading()
+          timerInterval = setInterval(() => {
+            const content = Swal.getContent()
+            if (content) {
+              const b = content.querySelector('b')
+              if (b) {
+                b.textContent = Swal.getTimerLeft()
+              }
+            }
+          }, 100)
+        },
+        willClose: () => {
+          clearInterval(timerInterval)
+        }
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            if (celular()) {
+                window.open(urlMovil+numero.value);
+            } else {
+                window.open(urlWeb+numero.value);
+            }
+        }
+      })
+   
         		
-        	}, 500);
+    }
     
 })
 
 
 
-// function isMobile() {
-// 	if (sessionStorage.desktop) return false;
-// 	else if (localStorage.mobile) return true;
-// 	var mobile = [
-// 		'iphone',
-// 		'ipad',
-// 		'android',
-// 		'blackberry',
-// 		'nokia',
-// 		'opera mini',
-// 		'windows mobile',
-// 		'windows phone',
-// 		'iemobile'
-// 	];
-// 	for (var i in mobile)
-// 		if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0)
-// 			return true;
-// 	return false;
-// }
-
-// const formulario = document.querySelector('#reserva');
-// const buttonSubmit = document.querySelector('#submit');
-// const urlDesktop = 'https://web.whatsapp.com/';
-// const urlMobile = 'whatsapp://';
-// const telefono = '51993238940';
-
-// formulario.addEventListener('submit', (event) => {
-// 	event.preventDefault();
-// 	buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>';
-// 	buttonSubmit.disabled = true;
-// 	setTimeout(() => {
-// 		let nombre = document.querySelector('#nombre').value;
-// 		let apellido = document.querySelector('#apellido').value;
-// 		let fecha = document.querySelector('#fecha').value;
-// 		var habitacion=document.getElementById("habitacion").value;
-
-// 		// let email = document.querySelector('#email').value;
-// 		let mensaje =
-// 			'send?phone=' +
-// 			telefono +
-// 			'&text=Reservar Habitacion :%0ANombre : ' +
-// 			nombre + ' ' + apellido +
-// 			'%0AFecha : ' +
-// 			fecha +
-// 			'%0AHabitación :%0A' +
-// 			habitacion +
-// 			'';
-// 		if (isMobile()) {
-// 			window.open(urlMobile + mensaje, '_blank');
-// 		} else {
-// 			window.open(urlDesktop + mensaje, '_blank');
-// 		}
-// 		buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp';
-// 		buttonSubmit.disabled = false;
-// 	}, 500);
-// });
